2cb89907b816cc0731f27bdf0baa8f9f6131bb93
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-07-02 13:37:09 -0400

    chore: create and organize basic files

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,16 @@
+// src/pages/Main.jsx
+
+/* 
+layout
+
+header (logo, name, anchor links: about / projects / journey / art / contact) (sticky)
+
+"hero" panel (basic blurb, image)
+about (+ tech skills) +link to detailed about page
+projects (3 main projects) +link to detailed projects page
+journey (currently + brief timeline) +link to detailed journey page
+hobbies (some interests) +link to detailed hobbies page
+contact (github, linkedin, email, resume)
+
+footer (name and link to github)
+*/

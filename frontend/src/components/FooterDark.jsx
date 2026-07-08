833f6d49bc12b507f8313e72424b327c3e00c254
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-07-08 13:09:36 -0400

    style: set up dark footer for light detail pages

--- frontend/src/components/FooterDark.jsx
+++ frontend/src/components/FooterDark.jsx
@@ -4,9 +4,24 @@
 function FooterDark() {
     return (
         <div className="dark-footer">
-            <div className="label">Next</div>
+            <div className="footer-left">
+                <div className="footer-label accented-label">Next</div>
+                <h3 className="next-section dark-next">[TODO] →</h3>
+            </div>
+            <div className="footer-right">
+                <a href="#" className="nav-arrow dark-nav">→</a>
+            </div>
         </div>
     )
 }
 
 export default FooterDark
+
+/* 
+TODO:
+conditionally display "next-section" title
+based on the page currently displayed
+title should be the next panel section from the main page
+
+conditionally render link for nav-arrow
+*/

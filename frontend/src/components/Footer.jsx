244081370f96505faa65006e8d72fc1041a84fb5
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-07-13 14:52:13 -0400

    feat: add conditional footer text/link functionality

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,27 +1,18 @@
 // src/components/Footer.jsx
 
 /* footer for non-project detail pages */
-function Footer({ variant }) {
+function Footer({ variant, nextSection }) {
     return (
         <div className={`${variant}-footer`}>
             <div className="footer-left">
                 <div className={`footer-label ${variant}-label`}>Next</div>
-                <h3 className={`next-section ${variant}-next`}>[TODO] →</h3>
+                <h3 className={`next-section ${variant}-next`}>{nextSection.label} →</h3>
             </div>
             <div className="footer-right">
-                <a href="#" className={`nav-arrow ${variant}-nav`}>→</a>
+                <a href={nextSection.href} className={`nav-arrow ${variant}-nav`}>→</a>
             </div>
         </div>
     )
 }
 
 export default Footer
-
-/* 
-TODO:
-conditionally display "next-section" title
-based on the page currently displayed
-title should be the next panel section from the main page
-
-conditionally render link for nav-arrow
-*/

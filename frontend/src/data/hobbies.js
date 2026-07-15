7ead5c92f44deea025edd998e5a93b6adcc5a170
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-07-15 16:39:31 -0400

    fix: preview image no longer an array

--- frontend/src/data/hobbies.js
+++ frontend/src/data/hobbies.js
@@ -6,25 +6,25 @@ import placeholderImage from "../assets/placeholder-hover.png";
 /* hobbies data */
 const hobbies = [
   {
-    image: [placeholderImage],
+    image: placeholderImage,
     title: "Placeholder title 1",
     type: "Digital art",
     year: "info",
   },
   {
-    image: [placeholderImage],
+    image: placeholderImage,
     title: "Placeholder title 2",
     type: "Crochet",
     year: "info",
   },
   {
-    image: [placeholderImage],
+    image: placeholderImage,
     title: "Placeholder title 3",
     type: "Baking",
     year: "info",
   },
   {
-    image: [placeholderImage],
+    image: placeholderImage,
     title: "Placeholder title 4",
     type: "Cats",
     year: "info",
--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,6 +1,9 @@
 // src/data/projects.js
 
 // imports
+import messagingPreview from "../assets/projects/messagingPreview.png";
+
+// TEMPORARY //
 import detailImage1 from "../assets/placeholder-hover.png";
 import detailImage2 from "../assets/placeholder-hover.png";
 import imagePreview from "../assets/placeholder-preview.png";
@@ -9,26 +12,27 @@ import imagePreview from "../assets/placeholder-preview.png";
 const projects = [
   {
     featured: true,
-    slug: "test-project-1",
+    slug: "hack-the-planet",
     number: "01",
-    title: "Test Project 1",
-    blurb: "This is a project test blurb",
-    role: "info",
-    timeline: "info",
-    year: "info",
-    type: "info",
-    liveLink: "https://github.com/MK-DlR",
-    codeLink: "https://github.com/MK-DlR?tab=repositories",
-    preview: [imagePreview],
+    title: "Hack the Planet",
+    blurb: "Full-stack cyberpunk messaging web app.",
+    role: "Solo developer",
+    timeline: "Feb - March 2026",
+    year: "2026",
+    type: "Full-stack web app",
+    liveLink: "https://messaging-app-bice-nine.vercel.app/",
+    codeLink: "https://github.com/MK-DlR/messaging-app",
+    preview: messagingPreview,
     stack: [
-      "React",
-      "Express",
-      "Node.js",
-      "Prisma",
-      "PostgreSQL",
+      "Authentication",
       "Design",
-      "Extra long tag",
-      "Additional long tag",
+      "Express",
+      "JavaScript",
+      "Node.js",
+      "PostgreSQL",
+      "Prisma",
+      "React",
+      "Vite",
     ],
     problem: "info",
     built: "info",
@@ -49,7 +53,7 @@ const projects = [
     type: "info",
     liveLink: "https://github.com/MK-DlR",
     codeLink: "https://github.com/MK-DlR?tab=repositories",
-    preview: [imagePreview],
+    preview: imagePreview,
     stack: ["React"],
     problem: "info",
     built: "info",
@@ -70,7 +74,7 @@ const projects = [
     type: "info",
     liveLink: "https://github.com/MK-DlR",
     codeLink: "https://github.com/MK-DlR?tab=repositories",
-    preview: [imagePreview],
+    preview: imagePreview,
     stack: ["Express"],
     problem: "info",
     built: "info",
@@ -91,7 +95,7 @@ const projects = [
     type: "info",
     liveLink: "https://github.com/MK-DlR",
     codeLink: "https://github.com/MK-DlR?tab=repositories",
-    preview: [imagePreview],
+    preview: imagePreview,
     stack: ["Design"],
     problem: "info",
     built: "info",
@@ -103,12 +107,3 @@ const projects = [
 ];
 
 export default projects;
-
-/*
-TODO:
-fill in with real project data
-
-note:
-"stack" will also function as the tags
-on AllProjects.jsx (should match the given filter button options)
-*/

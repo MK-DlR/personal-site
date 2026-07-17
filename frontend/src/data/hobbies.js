0dbcd3cf8dd0375073a8eecc1a2667f2051de3d5
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-07-17 11:37:42 -0400

    feat: add real hobby data

--- frontend/src/data/hobbies.js
+++ frontend/src/data/hobbies.js
@@ -2,43 +2,95 @@
 
 // imports
 import hobbiesArt1 from "../assets/hobbies/hobbiesArt1.png";
+
 import hobbiesBaking1 from "../assets/hobbies/hobbiesBaking1.jpg";
 import hobbiesBaking2 from "../assets/hobbies/hobbiesBaking2.jpg";
+
 import hobbiesCat1 from "../assets/hobbies/hobbiesCat1.jpg";
+import hobbiesCat2 from "../assets/hobbies/hobbiesCat2.jpg";
+
+import hobbiesCrochet1 from "../assets/hobbies/hobbiesCrochet1.jpg";
+import hobbiesCrochet2 from "../assets/hobbies/hobbiesCrochet2.jpg";
+import hobbiesCrochet3 from "../assets/hobbies/hobbiesCrochet3.jpg";
+import hobbiesCrochet4 from "../assets/hobbies/hobbiesCrochet4.jpg";
+import hobbiesCrochet5 from "../assets/hobbies/hobbiesCrochet5.jpg";
 
 // hobbies data
 const hobbies = [
   {
     id: 1,
-    image: hobbiesArt1,
-    title: "Placeholder title 1",
-    type: "Digital Art",
-    year: "info",
-    url: "info",
+    image: hobbiesCat2,
+    title: "Kraken as a lap warmer",
+    type: "Cats",
+    year: "2026",
   },
   {
     id: 2,
-    image: hobbiesBaking1,
-    title: "Placeholder title 2",
+    image: hobbiesCrochet1,
+    title: "Balaclava turtleneck hoodie",
     type: "Crochet",
-    year: "info",
-    url: "https://www.ravelry.com/",
+    year: "2026",
+    url: "https://www.ravelry.com/projects/mk-dlr/balaclava-turtleneck-hoodie",
   },
   {
     id: 3,
-    image: hobbiesBaking2,
-    title: "Placeholder title 3",
-    type: "Baking",
-    year: "info",
-    url: "info",
+    image: hobbiesCrochet2,
+    title: "Balaclava turtleneck hoodie",
+    type: "Crochet",
+    year: "2026",
+    url: "https://www.ravelry.com/projects/mk-dlr/balaclava-turtleneck-hoodie",
   },
   {
     id: 4,
     image: hobbiesCat1,
-    title: "Placeholder title 4",
+    title: "Kraken as a coding assistant",
     type: "Cats",
-    year: "info",
-    url: "info",
+    year: "2026",
+  },
+  {
+    id: 5,
+    image: hobbiesCrochet3,
+    title: "Fingerless paw gloves",
+    type: "Crochet",
+    year: "2026",
+    url: "https://www.ravelry.com/projects/mk-dlr/fingerless-paw-gloves",
+  },
+  {
+    id: 6,
+    image: hobbiesCrochet4,
+    title: "Kitty desk buddy",
+    type: "Crochet",
+    year: "2026",
+    url: "https://www.ravelry.com/projects/mk-dlr/kitty-desk-buddy",
+  },
+  {
+    id: 6,
+    image: hobbiesCrochet5,
+    title: "Amigurumi corgi puppy",
+    type: "Crochet",
+    year: "2026",
+    url: "https://www.ravelry.com/projects/mk-dlr/little-biscuit-the-corgi-puppy",
+  },
+  {
+    id: 7,
+    image: hobbiesBaking1,
+    title: "Deep dish apple pie",
+    type: "Baking",
+    year: "2025",
+  },
+  {
+    id: 7,
+    image: hobbiesBaking2,
+    title: "Chocolate chip banana muffins",
+    type: "Baking",
+    year: "2025",
+  },
+  {
+    id: 8,
+    image: hobbiesArt1,
+    title: "Yakuza/RGG fanart",
+    type: "Digital Art",
+    year: "2025",
   },
 ];
 
